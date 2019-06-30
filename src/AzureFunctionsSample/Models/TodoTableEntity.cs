397e352c2d52f3de56c05071de44cd01a2ab0db4
Jorge Candeias <candeias@fastmail.fm>
2019-06-30 13:20:32 +0100

    Added TodoTableEntity

---
+++ src/AzureFunctionsSample/Models/TodoTableEntity.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+using System;
+
+namespace AzureFunctionsSample.Models
+{
+    public class TodoTableEntity : TableEntity
+    {
+        public DateTime Created { get; set; }
+        public string Description { get; set; }
+        public bool IsCompleted { get; set; }
+    }
+}
