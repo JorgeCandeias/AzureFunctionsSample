2ca40f222da98529682f808f4ba198f54384faac
Jorge Candeias <candeias@fastmail.fm>
2019-06-30 13:29:58 +0100

    Added TodoExtensions

---
+++ src/AzureFunctionsSample/Models/TodoExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace AzureFunctionsSample.Models
+{
+    public static class TodoExtensions
+    {
+        public static TodoTableEntity ToTableEntity(this Todo todo) =>
+            new TodoTableEntity
+            {
+                PartitionKey = "TODO",
+                RowKey = todo.Id.ToString(),
+                Created = todo.Created,
+                IsCompleted = todo.IsCompleted,
+                Description = todo.Description
+            };
+
+        public static Todo ToTodo(this TodoTableEntity entity) =>
+            new Todo
+            {
+                Id = Guid.Parse(entity.RowKey),
+                Created = entity.Created,
+                IsCompleted = entity.IsCompleted,
+                Description = entity.Description
+            };
+    }
+}
